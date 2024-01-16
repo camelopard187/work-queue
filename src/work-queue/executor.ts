f3032c347c5f36cbe98ae4a4a1de80c88c82443a
Dmytro Fedkiv <dmytro.fedkiv@userway.org>
2024-01-16 07:53:02 +0100

    Duplicate SimpleWorkQueueExecutor

--- src/work-queue/executor.ts
+++ src/work-queue/executor.ts
@@ -55,3 +55,44 @@ export class SimpleWorkQueueExecutor<Input, Output>
     return fulfilled;
   }
 }
+
+export class SimpleWorkQueueExecutorDuplicate<Input, Output>
+  implements WorkQueueExecutor<Input, Output>
+{
+  private input: Input[] = [];
+
+  public constructor(
+    private readonly workers: number,
+    private readonly size: number
+  ) {}
+
+  public push(...input: Input[]): this {
+    if (this.size < this.input.length + input.length)
+      throw new QueueLimitError("Queue is full");
+
+    return this.input.push(...input), this;
+  }
+
+  public async process(
+    handler: QueueHandler<Input, Output>
+  ): Promise<Output[]> {
+    const pending: Promise<void>[] = [];
+    const fulfilled: Output[] = [];
+
+    for (const input of this.input) {
+      const task: Promise<void> = handler(input, this)
+        .then((output) => fulfilled.push(output))
+        .then(() => {
+          pending.splice(pending.indexOf(task), 1);
+        });
+
+      pending.push(task);
+
+      while (pending.length >= this.workers) {
+        await Promise.race(pending);
+      }
+    }
+
+    return fulfilled;
+  }
+}
